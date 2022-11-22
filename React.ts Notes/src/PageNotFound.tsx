97a30bc106cbbb76aa21a8bc1f9af5f424a294c8
CommanderGL <85912189+CommanderGL@users.noreply.github.com>
2022-11-22 18:09:32 +0000

    Fixed 404

--- React.ts Notes/src/PageNotFound.tsx
+++ React.ts Notes/src/PageNotFound.tsx
@@ -2,6 +2,6 @@ import './styles/PageNotFound.css'
 
 export default function PageNotFound() {
   return (
-    <h1>404 Page Not Found</h1>
+    <h1 className="pnf-txt">404 Page Not Found</h1>
   )
 }
